af711d7331dbd4d475661e5647c4c2299aeaa83a
Gino Messmer <messmer@medialesson.de>
2019-03-31 13:20:42 +0200

    Add remote fetch for meta context

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,40 @@ import HomePage from './components/pages/home';
 import moment from 'moment';
 import Footer from './components/partials/footer';
 import PrivacyPolicyPage from './components/pages/privacy';
+import { MetaContext } from './common/context/meta-context';
+import Meta from './data/meta';
 
 class App extends Component {
+
+  private remoteMeta: Meta = {
+    posts: [],
+    projects: []
+  }
+
+  async componentDidMount() {
+    let response = await fetch('https://ginomessmer.blob.core.windows.net/dump/sOpD.txt', {
+
+    });
+    if (response.ok) {
+      let data = (await response.json()) as Meta;
+      this.remoteMeta = data;
+    }
+  }
+
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Switch>
-            <Route path="/" exact component={HomePage} />
-            <Route path="/privacy" exact component={PrivacyPolicyPage} />
-          </Switch>
+      <MetaContext.Provider value={this.remoteMeta}>
+        <BrowserRouter>
+          <div>
+            <Switch>
+              <Route path="/" exact component={HomePage} />
+              <Route path="/privacy" exact component={PrivacyPolicyPage} />
+            </Switch>
 
-          <Footer />
-        </div>
-      </BrowserRouter>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </MetaContext.Provider>
     );
   }
 }
